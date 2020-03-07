662a97b02e8d36426cbc7c7527be7dc64c807892
Samantha Hangsan <shangsan@ucla.edu>
2020-03-06 20:09:32 -0800

    Smooth dim transition for LED 2; update formatting

--- led.ino
+++ led.ino
@@ -1,168 +1,135 @@
+///
+/// @file
+/// @brief ESP32 LED web server
+///
 
-// Load Wi-Fi library
-#include <WiFi.h>
 #include "Arduino.h"
+#include <WiFi.h>
 
-// Replace with your network credentials
-const char* ssid     = "SSID";
-const char* password = "PASSWORD";
+constexpr auto ssid           {"SSID"};                         // network credentials
+constexpr auto password       {"PASSWORD"};
+constexpr auto led1Pin        {26};                             // LED for on / off
+constexpr auto led2Pin        {27};                             // LED for pwm
+constexpr auto led2PwmChannel {0};
 
-// Set web server port number to 80
-WiFiServer server(80);
+static WiFiServer server(80);                                   // web server
+static String     header      {};                               // store HTTP request
+static auto       led1State   {false};
 
-// Variable to store the HTTP request
-String header;
+constexpr auto webHeader
+{
+    "<!DOCTYPE html><html>\n"
+    "<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n"
+    "<LINK rel='stylesheet' href='https://use.fontawesome.com/releases/v5.7.2/css/all.css' integrity='sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr' crossorigin='anonymous'/>\n"
+    "<link rel=\"icon\" href=\"data:,\">\n"
+    "<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n"
+    ".button { background-color: #000; border: none; color: white; padding: 16px 40px;\n"
+    "text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}\n"
+    ".button2 {background-color: #ade6e6; color: black;}\n"
+    ".slider {width: 300px; height: 50px;}</style></head>\n"
+    "<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js\"></script>\n"
+    "<body><h1>All of the Lights <i class='far fa-lightbulb'></i></h1>\n"
+};
 
-// Auxiliar variables to store the current output state
-String output26State = "off";
-String output27State = "off";
+constexpr auto webFooter
+{
+    "<script>var slider = document.getElementById(\"ledSlider\");\n"
+    "var ledP = document.getElementById(\"ledPos\"); ledP.innerHTML = slider.value;\n"
+    "slider.oninput = function() { slider.value = this.value; ledP.innerHTML = this.value; }\n"
+    "setInterval(function() { led(slider.value); }, 100); $.ajaxSetup({timeout:2000}); function led(pos) { \n"
+    "$.get(\"/?value=\" + pos + \"&\");{Connection: close};\n"
+    "}</script>\n"
+};
 
-// Assign output variables to GPIO pins
-const int output26 = 26;
-const int output27 = 27;
+void setup()
+{
+    Serial.begin(115200);
+    pinMode(led1Pin, OUTPUT);                                   // configure LED 1 (GPIO)
+    digitalWrite(led1Pin, LOW);
 
-// Decode HTTP GET value
-String valueString = String(0);
-int pos1 = 0;
-int pos2 = 0;
+    ledcSetup(led2PwmChannel, 5000, 8);                         // configure LED 2 (PWM)
+    ledcAttachPin(led2Pin, led2PwmChannel);
 
-const int freq = 5000;
-const int ledChannel = 0;
-const int resolution = 8;
+    Serial.print("Connecting to " + String{ssid});              // configure WIFI
+    WiFi.begin(ssid, password);
+    while(WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
+    }
 
-void setup() {
-  Serial.begin(115200);
-  // Initialize the output variables as outputs
-  pinMode(output26, OUTPUT);
-  // Set outputs to LOW
-  digitalWrite(output26, LOW);
-  // configure LED PWM functionalities
-  ledcSetup(ledChannel, freq, resolution);
-  // attach channel to GPIO to be controlled
-   ledcAttachPin(output27, ledChannel);
-  
-  // Connect to Wi-Fi network with SSID and password
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  // Print local IP address and start web server
-  Serial.println("");
-  Serial.println("WiFi connected.");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-  server.begin();
+    Serial.println("\nWiFi connected!\nIP address: " + WiFi.localIP());
+    server.begin();
 }
 
-void loop(){
-  WiFiClient client = server.available();   // Listen for incoming clients
+void loop()
+{
+    WiFiClient client {server.available()};                     // listen for incoming clients
 
-  if (client) {                             // If a new client connects,
-    Serial.println("New Client.");          // print a message out in the serial port
-    String currentLine = "";                // make a String to hold incoming data from the client
-    while (client.connected()) {            // loop while the client's connected
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        Serial.write(c);                    // print it out the serial monitor
-        header += c;
-        if (c == '\n') {                    // if the byte is a newline character
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
-          if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println("Connection: close");
-            client.println();
-            
-            // turns the GPIOs on and off
-            if (header.indexOf("GET /26/on") >= 0) {
-              Serial.println("GPIO 26 on");
-              output26State = "on";
-              digitalWrite(output26, HIGH);
-            } else if (header.indexOf("GET /26/off") >= 0) {
-              Serial.println("GPIO 26 off");
-              output26State = "off";
-              digitalWrite(output26, LOW);
-            } else if (header.indexOf("GET /27/on") >= 0) {
-              Serial.println("GPIO 27 on");
-              output27State = "on";
-              digitalWrite(output27, HIGH);
-            } else if (header.indexOf("GET /27/off") >= 0) {
-              Serial.println("GPIO 27 off");
-              output27State = "off";
-              digitalWrite(output27, LOW);
+    if(client)                                                  // when a new client connects
+    {
+        String currentLine {""};                                // hold incoming data from the client
+        while(client.connected())                               // while the client is connected
+        {
+            if(client.available())                              // if bytes are available
+            {
+                const char c {client.read()};                   // read a byte
+                header += c;
+
+                if(c == '\n')                                   // if the byte is a newline character
+                {                                               // if you get two newline characters in a row,
+                    if(!currentLine.length())                   // that's the end of the client HTTP request, so send a response:
+                    {
+                        client.println("HTTP/1.1 200 OK");
+                        client.println("Content-type:text/html");
+                        client.println("Connection: close");
+                        client.println();
+
+                        if(header.indexOf("GET /26/on") >= 0)
+                        {
+                            led1State = true;
+                            digitalWrite(led1Pin, HIGH);
+                        }
+                        else if(header.indexOf("GET /26/off") >= 0)
+                        {
+                            led1State = false;
+                            digitalWrite(led1Pin, LOW);
+                        }
+
+                        const auto led2Pwm {String(ledcRead(led2PwmChannel))};
+                        const auto led1Cmd {String{led1State ? "off" : "on"}};
+
+                        client.println(webHeader);
+                        client.print("<p>GPIO 26 - State " + led1Cmd + "</p>");    // info for LED1
+                        client.println("<p><a href=\"/26/" + led1Cmd + "\"><button class=\"button" + String{led1State ? "" : " button2"} + "\">" + led1Cmd + "</i></button></a></p>");
+                        client.println("<p>GPIO 27 - Brightness: <p id=ledPos>" + led2Pwm + "</p></p>");
+                        client.println("<input type=\"range\" min=\"0\" max=\"255\" class=\"slider\" id=\"ledSlider\" onchange=\"led(this.value)\" value=\"" + led2Pwm + "\"/>");
+                        client.println(webFooter);
+
+                        if(header.indexOf("GET /?value=")>=0)
+                        {
+                            const auto pos1 {header.indexOf('=')};
+                            const auto pos2 {header.indexOf('&')};
+                            const auto val  {header.substring(pos1 + 1, pos2)};
+                            ledcWrite(led2PwmChannel, val.toInt());
+                        }
+
+                        client.println("</body></html>\n");
+                        break;
+                    }
+                    else                                        // if you get a newline, then clear currentLine
+                    {
+                        currentLine = "";
+                    }
+                }
+                else if(c != '\r')
+                {                                               // if you got anything else but a carriage return character,
+                    currentLine += c;                           // add it to the end of the currentLine
+                }
             }
-
-            valueString = String(ledcRead(ledChannel));
-            
-            // Display the HTML web page
-            client.println("<!DOCTYPE html><html>");
-            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-            client.println("<LINK rel='stylesheet' href='https://use.fontawesome.com/releases/v5.7.2/css/all.css' integrity='sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr' crossorigin='anonymous'/>");
-            client.println("<link rel=\"icon\" href=\"data:,\">");
-            // CSS to style the on/off buttons 
-            // Feel free to change the background-color and font-size attributes to fit your preferences
-            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
-            client.println(".button { background-color: #000; border: none; color: white; padding: 16px 40px;");
-            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
-            client.println(".button2 {background-color: #ade6e6; color: black;}");
-            client.println(".slider {width: 300px; height: 50px;}</style></head>");
-            client.println("<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js\"></script>");
-            
-            // Web Page Heading
-            client.println("<body><h1>All of the Lights <i class='far fa-lightbulb'></i></h1>");
-            
-            // Display current state, and ON/OFF buttons for GPIO 26 
-            client.print("<p>GPIO 26 - State " + output26State + "</p>");
-            // If the output26State is off, it displays the ON button       
-            if (output26State=="off") {
-              client.println("<p><a href=\"/26/on\"><button class=\"button\">ON</i></button></a></p>");            } else {
-              client.println("<p><a href=\"/26/off\"><button class=\"button button2\">OFF</button></a></p>");
-            } 
-               
-            // Display current brightness for GPIO 27  
-            client.println("<p>GPIO 27 - Brightness: <p id=ledPos>"+ valueString + "</p></p>");
-
-            client.println("<input type=\"range\" min=\"0\" max=\"255\" class=\"slider\" id=\"ledSlider\" onchange=\"led(this.value)\" value=\""+valueString+"\"/>");
-            client.println("<script>var slider = document.getElementById(\"ledSlider\");");
-            client.println("var ledP = document.getElementById(\"ledPos\"); ledP.innerHTML = slider.value;");
-            client.println("slider.oninput = function() { slider.value = this.value; ledP.innerHTML = this.value; }");
-            client.println("$.ajaxSetup({timeout:1000}); function led(pos) { ");
-            client.println("$.get(\"/?value=\" + pos + \"&\"); {Connection: close};}</script>");
-
-            //GET /?value=180& HTTP/1.1
-            if(header.indexOf("GET /?value=")>=0) {
-              pos1 = header.indexOf('=');
-              pos2 = header.indexOf('&');
-              valueString = header.substring(pos1+1, pos2);
-              ledcWrite(ledChannel,valueString.toInt());
-              Serial.println(valueString);
-            }
-            
-            client.println("</body></html>");
-            
-            // The HTTP response ends with another blank line
-            client.println();
-            // Break out of the while loop
-            break;
-          } else { // if you got a newline, then clear currentLine
-            currentLine = "";
-          }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
         }
-      }
+
+        header = "";
+        client.stop();
     }
-    // Clear the header variable
-    header = "";
-    // Close the connection
-    client.stop();
-    Serial.println("Client disconnected.");
-    Serial.println("");
-  }
 }
